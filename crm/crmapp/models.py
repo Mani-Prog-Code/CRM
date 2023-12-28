9fc53ec6942c7e3869800d88f4b98d20657b8f7e
Mani-Prog-Code <154578077+Mani-Prog-Code@users.noreply.github.com>
2023-12-28 10:28:22 +0530

    Add files via upload

---
+++ crm/crmapp/models.py
@@ -0,0 +1,43 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Record(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    first_name = models.CharField(max_length=100)
+
+    last_name = models.CharField(max_length=100)
+
+    email = models.CharField(max_length=255)
+
+    phone = models.CharField(max_length=20)
+
+    address = models.CharField(max_length=300)
+
+    city = models.CharField(max_length=255)
+
+    province = models.CharField(max_length=200)
+
+    country = models.CharField(max_length=125)
+
+
+    def __str__(self):
+
+        return self.first_name + "   " + self.last_name
+
+
+
+
+
+
+
+
+
+
+
+
+
